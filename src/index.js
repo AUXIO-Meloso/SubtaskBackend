6b33a7332d49838a2f32defa38ea5057aafde39f
Meloso <groove.street.x@hotmail.com>
2020-03-07 14:34:44 -0300

    Primeiro e unico commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config(); // Variáveis de Ambiente
+const cors = require('cors');
 const express = require('express'); // Controles de rotas 
 
 const app = express(); // Criando uma aplicação node
 
+app.use(cors());
 app.use(express.json()); // Dizendo a aplicação usar json como estrutura de dados entre o Back e Front
 app.use(express.urlencoded({ extended: false })); // Troca de dados pela url 
 require('./app/routes/index')(app);
